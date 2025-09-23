31e5c97789c8374b4d263ef72a889b7c7ceec3e4
Md: Farok Ahmed <mdfarokahmed280@gmail.com>
2025-09-23 11:27:58 +0600

    client component to server component index company page

--- src/components/pages/index-company/handleSubmit.ts
+++ src/components/pages/index-company/handleSubmit.ts
@@ -1,12 +1,15 @@
-"use client"
+"use server";
+
 type ContactFormFields = {
   fullName: string;
   email: string;
 };
- export async function handleSubmit(formData: FormData) {
-    const data: ContactFormFields = {
-      fullName: formData.get("fullName") as string,
-      email: formData.get("email") as string,
-    };
-    console.log("Form submitted:", data);
-  }
+
+export async function handleSubmit(formData: FormData) {
+  const data: ContactFormFields = {
+    fullName: formData.get("fullName") as string,
+    email: formData.get("email") as string,
+  };
+
+  console.log("Form submitted:", data);
+}
